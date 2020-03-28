3254a93aceb63b54fed4a32c5154b62dfd971f52
Shijing Liu <liushijing123@gmail.com>
2020-03-28 16:47:53 -0400

    set up question answer functionality

--- would-you-rather/src/components/PollDetails.js
+++ would-you-rather/src/components/PollDetails.js
@@ -1,13 +1,36 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import TitleBar from './TitleBar'
-import { formatDate } from '../utils/api'
-// import FaCheck from 'react-icons/lib/check'
+import { formatDate } from '../utils/helpers'
+import { FaCheck } from 'react-icons/fa'
+import { handleSaveQuestionAnswer } from '../actions/shared'
 
 class PollDetails extends Component {
+    state = {
+        selectedOption: ''
+    }
+
+    selectedRadio = (e) => {
+        this.setState({
+            selectedOption: e.target.value
+        })
+    }
+
+    submitAnswer = (e) => {
+        e.preventDefault()
+
+        const { authedUser, saveQuestionAnswer } = this.props
+        const answer = this.state.selectedOption
+        saveQuestionAnswer(authedUser, answer)
+    }
+
     render() {
-        const { avatar, timestamp, username, optionOne, optionTwo } = this.props
-        
+        const { question, authorAvatar, timestamp, author, optionOne, optionTwo, answered, isOneAnswered, isTwoAnswered } = this.props
+        const optionOneVotes = question.optionOne.votes.length
+        const optionTwoVotes = quesiton.optionTwo.votes.length
+        const optionOnePercetage = (optionOneVotes / (optionOneVotes + optionTwoVotes) * 100).toFixed(2)
+        const optionTwoPercentage = (optionTwoVotes / (optionOneVotes + optionTwoVotes) * 100).toFixed(2) 
+
         return (
             <Fragment>
                 <TitleBar />
@@ -15,30 +38,71 @@ class PollDetails extends Component {
                     <div className="form-header">
                         <p className="form-title">Would You Rather</p>
                     </div>
-                    <div className="form-body no-bottom-round">
-                        <div className="radio_container-div">
-                            <label className="radio_container">
-                                <span className="input_radio">{optionOne}</span>
-                                <input className="hide" type="radio" defaultChecked="checked" name="select_option"/>
-                                <span className="checkmark"></span>
-                            </label>
-                            <label className="radio_container">
-                                <span className="input_radio">{optionTwo}</span>
-                                <input className="hide" type="radio" name="select_option"/>
-                                <span className="checkmark"></span>
-                            </label>
-                        </div>
-                        <button className="button" onClick={this.handleLogin}>Submit</button>
-                    </div>
+                    {
+                        answered
+                        ? (
+                            <div className='form-body no-bottom-round'>
+                                <ul className='no-padding no-margin'>
+                                    <li className="fix-answered-li full-width">
+                                        <span className={isOneAnswered ? 'answered' : ''}>{optionOne}</span>
+                                        {isOneAnswered ? <FaCheck className='padding-left answered'/> : null}
+                                        <span className='vote-result'>{`${optionOneVotes} vote(s) | ${optionOnePercetage}%`}</span>
+                                    </li>
+                                    <li className="no-padding fix-answered full-width">
+                                        <div className='or-seperator'>
+                                            <hr/>
+                                            <p className='inline-p'>OR</p>
+                                            <hr/>
+                                        </div>
+                                    </li>
+                                    <li className="no-padding fix-answered-li full-width">
+                                        <span className={isTwoAnswered ? 'answered' : ''}>{optionTwo}</span>
+                                        {isTwoAnswered ? <FaCheck className='padding-left answered'/> : null}
+                                        <span className='vote-result'>{`${optionTwoVotes} vote(s) | ${optionTwoPercentage}%`}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        )
+                        : (
+                            <form className="form-body no-bottom-round">
+                                <div className="radio_container-div">
+                                    <label className="radio_container">
+                                        <span className="input_radio">{optionOne}</span>
+                                        <input
+                                            className='hide'
+                                            type='radio'
+                                            name='select_option'
+                                            value='optionOne'
+                                            onclick={this.selectedRadio}
+                                        />
+                                        <span className="checkmark"></span>
+                                    </label>
+
+                                    <label className="radio_container">
+                                        <span className="input_radio">{optionTwo}</span>
+                                        <input 
+                                            className='hide'
+                                            type='radio'
+                                            name='select_option'
+                                            value='optionTwo'
+                                            onClick={this.selectedRadio}
+                                        />
+                                        <span className='checkmark'></span>
+                                    </label>
+                                </div>
+                                <button className="button" onClick={this.submitAnswer}>Submit</button>
+                            </form>
+                        )
+                    }
                     <div className="user-details">
                         <ul className="user-detail-ul nav nav-account block">
                             <li className="user-info-li inline-block">
                                 <img
-                                    src={avatar}
-                                    alt={`Avatar of ${avatar}`}
+                                    src={authorAvatar}
+                                    alt={`Avatar of ${author}`}
                                     className='scale-down-mid profile-pic vertical-align' 
                                 />
-                                <span className="padding-left">{username}</span>
+                                <span className="padding-left">{author}</span>
                             </li>
                             <li className="time-stamp user-info-li block">
                                 { timestamp }
@@ -53,21 +117,40 @@ class PollDetails extends Component {
 
 function mapStateToProps ({authedUser, questions, users}, props) {
     const { question_id } = props.match.params
-
     const question = questions[question_id]
-    const avatar = users[question.author].avatarURL
-    const username = users[question.author].id
+    const authorAvatar = users[question.author].avatarURL
+    const author = users[question.author].id
     const timestamp = formatDate(question.timestamp)
     const optionOne = question.optionOne.text
     const optionTwo = question.optionTwo.text
+    const isOneAnswered = question.optionOne.votes.includes(authedUser)
+    const isTwoAnswered = question.optionTwo.votes.includes(authedUser)
+    const answered = isOneAnswered || isTwoAnswered
 
     return {
-        avatar,
-        username,
+        authorAvatar,
+        author,
         timestamp,
         optionOne,
         optionTwo,
+        answered,
+        isOneAnswered, 
+        isTwoAnswered, 
+        question, 
+        users,
+        questions,
+        authedUser,
+        question_id,
     }
 }
 
-export default connect(mapStateToProps)(PollDetails)
+function mapDispatchToProps(dispatch, props) {
+    const { question_id } = props.match.params
+    return {
+        saveQuestionAnswer: (authedUser, answer) => {
+            dispatch(handleSaveQuestionAnswer(authedUser, question_id, answer))
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PollDetails)
