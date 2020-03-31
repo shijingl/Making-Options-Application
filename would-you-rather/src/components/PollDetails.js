ea64edf8f3852add08c8b34c59ff847141517c51
Shijing Liu <liushijing123@gmail.com>
2020-03-31 11:37:14 -0400

    fix a bug in styling

--- would-you-rather/src/components/PollDetails.js
+++ would-you-rather/src/components/PollDetails.js
@@ -24,90 +24,86 @@ class PollDetails extends Component {
         savePollAnswer(answer)
     }
 
-    render() {
+    render () {
         const { poll, authorAvatar, timestamp, author, optionOne, optionTwo, answered, isOneAnswered, isTwoAnswered } = this.props
         const optionOneVotes = poll.optionOne.votes.length
         const optionTwoVotes = poll.optionTwo.votes.length
-        const optionOnePercetage = (optionOneVotes / (optionOneVotes + optionTwoVotes) * 100).toFixed(2)
-        const optionTwoPercentage = (optionTwoVotes / (optionOneVotes + optionTwoVotes) * 100).toFixed(2) 
-
+        const optionOnePercentage = (optionOneVotes / (optionOneVotes + optionTwoVotes) * 100).toFixed(2)
+        const optionTwoPercentage = (optionTwoVotes / (optionOneVotes + optionTwoVotes) * 100).toFixed(2)
         return (
             <Fragment>
                 <TitleBar />
-                <div className="form margin poll-details-form">
-                    <div className="form-header">
-                        <p className="form-title">Would You Rather</p>
+                <div className='form margin poll-details-form'>
+                    <div className='form-header'>
+                        <p className='form-title'>Would You Rather</p>
                     </div>
                     {
                         answered
                         ? (
                             <div className='form-body no-bottom-round'>
-                                <ul className='no-padding no-margin'>
-                                    <li className='fix-answered-li full-width'>
-                                        <span className={isOneAnswered ? 'answered' : ''}>{optionOne}</span>
-                                        {isOneAnswered ? <FaCheck className='padding-left answered'/> : null}
-                                        <span className='vote-result'>{`${optionOneVotes} vote(s) | ${optionOnePercetage}%`}</span>
-                                    </li>
-                                    <li className="no-padding fix-answered full-width">
-                                        <div className='or-seperator'>
-                                            <hr/>
-                                            <p className='inline-p'>OR</p>
-                                            <hr/>
-                                        </div>
-                                    </li>
-                                    <li className='padding-bottom fix-answered-li full-width'>
-                                        <span className={isTwoAnswered ? 'answered' : ''}>{optionTwo}</span>
-                                        {isTwoAnswered ? <FaCheck className='padding-left answered'/> : null}
-                                        <span className='vote-result'>{`${optionTwoVotes} vote(s) | ${optionTwoPercentage}%`}</span>
-                                    </li>
-                                </ul>
+                            <ul className='no-padding no-margin'>
+                                <li className='fix-answered-li full-width'>
+                                    <span className={isOneAnswered ? 'answered' : ''}>{optionOne}</span>
+                                    {isOneAnswered ? <FaCheck className='padding-left answered'/> : null}
+                                    <span className='vote-result'>{`${optionOneVotes} vote(s) | ${optionOnePercentage}%`}</span>
+                                </li>
+                                <li className='no-padding fix-answered-li full-width'>
+                                    <div className='or-seperator'>
+                                        <hr/>
+                                        <p className='inline-p'>OR</p>
+                                        <hr/>
+                                    </div>
+                                </li>
+                                <li className='padding-bottom fix-answered-li full-width'>
+                                    <span className={isTwoAnswered ? 'answered' : ''}>{optionTwo}</span>
+                                    {isTwoAnswered ? <FaCheck className='padding-left answered'/> : null}
+                                    <span className='vote-result'>{`${optionTwoVotes} vote(s) | ${optionTwoPercentage}%`}</span>
+                                </li>
+                            </ul>
                             </div>
                         )
                         : (
-                            <form onSubmit={this.submitAnswer} className="form-body no-bottom-round">
-                                <div className="radio_container-div">
-                                    <label className="radio_container">
-                                        <span className="input_radio">{optionOne}</span>
-                                        <input
+                            <form onSubmit={this.submitAnswer} className='form-body no-bottom-round'>
+                                <div className='radio_container-div'>
+                                    <label className='radio_container'>
+                                        <span className='input_radio'>{optionOne}</span>
+                                        <input  
                                             className='hide'
-                                            type='radio'
-                                            name='select_option'
-                                            value='optionOne'
-                                            onClick={this.selectRadio}
-                                        />
-                                        <span className="checkmark"></span>
+                                            type="radio" 
+                                            name='select_option' 
+                                            value="optionOne"
+                                            onClick={this.selectRadio}/>
+                                        <span className='checkmark'></span>
                                     </label>
 
-                                    <label className="radio_container">
-                                        <span className="input_radio">{optionTwo}</span>
+                                    <label className='radio_container'>
+                                        <span className='input_radio'>{optionTwo}</span>
                                         <input 
-                                            className='hide'
-                                            type='radio'
-                                            name='select_option'
-                                            value='optionTwo'
-                                            onClick={this.selectRadio}
-                                        />
+                                            className='hide' 
+                                            type="radio" 
+                                            name='select_option' 
+                                            value="optionTwo"
+                                            onClick={this.selectRadio}/>
                                         <span className='checkmark'></span>
                                     </label>
                                 </div>
-                                <button className="button">Submit</button>
+                                <button className='button'>Submit</button>
                             </form>
-                        )
+                        ) 
                     }
-                    <div className="user-details">
-                        <ul className="user-detail-ul nav nav-account block">
-                            <li className="user-info-li inline-block">
-                                <img
-                                    src={authorAvatar}
-                                    alt={`Avatar of ${author}`}
-                                    className='scale-down-mid profile-pic vertical-align' 
-                                />
-                                <span className="padding-left">{author}</span>
-                            </li>
-                            <li className="time-stamp user-info-li block">
-                                { timestamp }
-                            </li>
-                        </ul>
+                    <div className='user-details'>
+                            <ul className='user-detail-ul nav nav-account block'>
+                                <li className='user-info-li inline-block'>
+                                    <img 
+                                        src={authorAvatar}
+                                        alt={`Avatar of ${author}`}
+                                        className='scale-down-mid profile-pic vertical-align'/>
+                                    <span className="padding-left">{author}</span>
+                                </li>
+                                <li className='time-stamp user-info-li block'>
+                                    { timestamp }
+                                </li>
+                            </ul>
                     </div>
                 </div>
             </Fragment>
@@ -120,7 +116,7 @@ function mapStateToProps ({authedUser, polls, users}, props) {
     const poll = polls[question_id]
     const authorAvatar = users[poll.author].avatarURL
     const author = users[poll.author].id
-    const timestamp = formatDate(poll.timestamp)
+    const timestamp = formatDate (poll.timestamp)
     const optionOne = poll.optionOne.text
     const optionTwo = poll.optionTwo.text
     const isOneAnswered = poll.optionOne.votes.includes(authedUser)
@@ -134,9 +130,9 @@ function mapStateToProps ({authedUser, polls, users}, props) {
         optionOne,
         optionTwo,
         answered,
-        isOneAnswered, 
-        isTwoAnswered, 
-        poll, 
+        isOneAnswered,
+        isTwoAnswered,
+        poll,
         users,
         polls,
         authedUser,
@@ -144,10 +140,10 @@ function mapStateToProps ({authedUser, polls, users}, props) {
     }
 }
 
-function mapDispatchToProps(dispatch, props) {
+function mapDispatchToProps (dispatch, props) {
     const { question_id } = props.match.params
     return {
-        savePollAnswer: (answer) => {
+        savePollAnswer : (answer) => {
             dispatch(handleSavePollAnswer(question_id, answer))
         }
     }
