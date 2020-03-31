104894b08f62b817f952698f4e7d40d11053078d
Shijing Liu <liushijing123@gmail.com>
2020-03-31 12:27:42 -0400

    refactor class components to stateless components

--- would-you-rather/src/components/Poll.js
+++ would-you-rather/src/components/Poll.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-class Poll extends Component {
-    render () {
-        const { poll } = this.props
+const Poll = props => {
+    const { poll } = props
 
-        if (poll === null) {
-            return <p>This poll doesn't exist</p>
-        }
-
-        const { optionOne, optionTwo } = poll 
-        const { id } = this.props
-
-        return (
-            <Link to={`/questions/${id}`} className='form margin poll-form'>
-                <div className='form-header'>
-                    <p className='form-title'>Would You Rather</p>
-                </div>
-                <div className='form-body'>
-                    <p className='optionOne'>{optionOne.text}</p>
-                    <div className='or-seperator'>
-                        <hr/>
-                        <p className='inline-p'>OR</p>
-                        <hr/>
-                    </div>
-                    <p className='optionTwo'>{optionTwo.text}</p>
-                </div>
-            </Link>
-        )
+    if (poll === null) {
+        return <p>This poll doesn't exist</p>
     }
+
+    const { optionOne, optionTwo } = poll 
+    const { id } = props
+
+    return (
+        <Link to={`/questions/${id}`} className='form margin poll-form'>
+            <div className='form-header'>
+                <p className='form-title'>Would You Rather</p>
+            </div>
+            <div className='form-body'>
+                <p className='optionOne'>{optionOne.text}</p>
+                <div className='or-seperator'>
+                    <hr/>
+                    <p className='inline-p'>OR</p>
+                    <hr/>
+                </div>
+                <p className='optionTwo'>{optionTwo.text}</p>
+            </div>
+        </Link>
+    )
 }
 
 function mapStateToProps({authedUser, polls}, { id }) {
